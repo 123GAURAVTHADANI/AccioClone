8e51b35852cb40e74489d99539df86da1df019a4
123GAURAVTHADANI <garv.thadani@gmail.com>
2024-11-19 21:38:41 +0530

    Revert "Revert "DOM Changes""
    This reverts commit 21fba2c6fb7764d2f529ca73210f79625175b819.

--- index.js
+++ index.js
@@ -1,17 +1,73 @@
+let tableElement = document.getElementById("table");
+let tbody = document.createElement("tbody");
+tableElement.setAttribute("border", "1px");
+tableElement.appendChild(tbody);
+
+let thead = document.createElement("thead");
+tableElement.appendChild(thead);
+thead.innerHTML = "<tr><th>Title</th><th>Price</th><th>Description</th></tr>";
+
 fetch("https://fakestoreapi.com/products")
   .then((res) => res.json())
   .then((data) =>
     data.forEach((item) => {
-      let createCardElement = document.createElement("div");
-      createCardElement.setAttribute("class", "card");
-      createCardElement.innerHTML = `
-      <h1>${item.title}</h1>
-      <img src=${item.image} alt=${item.title}/>
-      <p>${item.description}</p>
+      tbody.innerHTML += `
+      <tr>
+      <td>${item.title}</td>
+      <td>${item.price}</td>
+      <td>${item.description}</td>
+      </tr>
       `;
-      createCardElement.style.backgroundColor = "purple";
-      createCardElement.style.padding = "10px";
-      createCardElement.style.margin = "10px";
-      document.getElementById("productCard").append(createCardElement);
     })
   );
+// const colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00"];
+
+// let element = document.getElementsByClassName("container");
+// console.dir(element[0].children);
+// element.forEach((item, index) => {
+//   item.style.backgroundColor = colors[index % colors.length];
+//   item.style.color = "white";
+// });
+// console.log(element);
+
+// let paraElement = document.createElement("p");
+// let spanElement = document.createElement("span");
+// let btnElement = document.createElement("button");
+// let container = document.getElementById("container");
+// let inputField = document.createElement("input");
+// container.appendChild(inputField);
+// container.setAttribute("id", "container");
+
+// paraElement.innerHTML = "<b>Lorem Ipsum</b>";
+// btnElement.innerText = "Click Me";
+// spanElement.innerText = "I am span";
+
+// container.appendChild(paraElement);
+// container.appendChild(btnElement);
+// container.appendChild(spanElement);
+
+// spanElement.style.padding = "10px";
+// spanElement.style.backgroundColor = "purple";
+// // inputField.focus();
+
+// btnElement.addEventListener("click", () => {
+//   // container.removeChild(paraElement);
+//   // container.removeChild(spanElement);
+//   // container.classList.toggle("bgcolor");
+//   container.classList.add("bgcolors");
+// });
+
+// let tableElement = document.getElementById("table");
+// let tbody = document.createElement("tbody");
+// tableElement.appendChild(tbody);
+// const tr = document.createElement("tr");
+// const td1 = document.createElement("td");
+// tr.appendChild(td1);
+// tbody.appendChild(tr);
+// td1.innerText = "Hello";
+
+// tbody.innerHTML = `<tr>
+// <td>Gaurav</td>
+// <td>28</td>
+// <td>FSD</td>
+// </tr>`;

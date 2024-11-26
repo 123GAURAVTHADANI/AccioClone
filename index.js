90348575ea77a5c92bcc382a8e51c3ac9ec534e5
123GAURAVTHADANI <garv.thadani@gmail.com>
2024-11-26 22:04:24 +0530

    Changes

--- index.js
+++ index.js
@@ -1,52 +1,49 @@
-// localStorage.setItem("timestamp", new Date().getTime());
-// localStorage.setItem("user", "gaurav123");
-
-// console.log(localStorage.getItem("theme"));
-// setTimeout(() => {
-//   localStorage.clear();
-// }, 5000);
-// localStorage , store the data in key , value
-
-// sessionStorage.setItem("user", "accio");
-// sessionStorage.setItem("theme", "dark");
-
-// // console.log(sessionStorage.getItem("user"));
-
-// setTimeout(() => {
-//   sessionStorage.removeItem("user");
-// }, 5000);
-
-// let object1 = {
+// let obj = {
 //   name: "Gaurav",
+//   age: 28,
+// };
+// let obj2 = {
+//   name: "Stakshi",
 //   age: 20,
 // };
 
-// localStorage.setItem("user", JSON.stringify(object1));
+// let employees = [
+//   { name: "Gaurav", age: 28 },
+//   { name: "Stakshi", age: 20 },
+//   { name: "Vishal", age: 20 },
+//   { name: "Raju", age: 20 },
+//   { name: "Afreen", age: 20 },
+// ];
 
-// document.cookie = "username:gaurav;expiresIn:30 Novemeber 2024 ";
-
-// document.getElementById("container4").addEventListener("click", () => {
-//   console.log("Container 4 is clicked!!!");
-// });
-// document.getElementById("container3").addEventListener("click", () => {
-//   console.log("Container 3 is clicked!!!");
-// });
-
-// document.getElementById("container2").addEventListener("click", () => {
-//   console.log("Container 2 is clicked!!!");
-// });
-
-// document.getElementById("button").addEventListener("click", () => {
-//   console.log("Button is clicked!!!");
-// });
-
-// document.getElementById("parent").addEventListener("click", (event) => {
-//   if (event.target && event.target.matches("button#btn")) {
-//     console.log("Btn is clicked!!", event.target.textContent);
-//   } else if (event.target && event.target.matches("div")) {
-//     console.log("Div is clicked!!");
+// function sayHi(greet, greet2, type) {
+//   //   console.log(`Hi ${this.name} ${greet} ${greet2}`);
+//   switch (type) {
+//     case "first": {
+//       return `Hi ${this.name} ${greet} ${greet2}`;
+//       break;
+//     }
+//     case "second": {
+//       return `Hi ${this.name} ${greet} ${greet2}`;
+//       break;
+//     }
 //   }
-// });
-// {data.map}
+// }
 
-// console.log(true && 2);
+// // fnc.call(obj)
+// // console.log(sayHi.call(obj2));
+
+// // console.log(sayHi.call(employees[1]));
+
+// // sayHi(employees[0]);
+
+// // employees.forEach((item, index) => {
+// //   if (index <= 2) {
+// //     console.log(sayHi.apply(item, ["good morning", "say goo bye!", "first"]));
+// //   } else {
+// //     console.log(
+// //       sayHi.apply(item, ["good afternoon", "say good bye!", "second"])
+// //     );
+// //   }
+// // });
+
+
